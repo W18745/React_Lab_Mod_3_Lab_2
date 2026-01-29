63591733489f5682ac5c285acf5fcf44c2d56927
DistantCandle <loftuswp2@gmail.com>
2026-01-29 01:48:53 -0500

    init state for data handle

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -2,36 +2,41 @@ import React, { useState } from 'react';
 import './FeedbackForm.css'; // Import CSS for styling
 
 const FeedbackForm = () => {
+    const [formData, setFormData] = useState({
+        name:'',
+        email:'',
+        feedback:''
+    })
 
-  return (
+    return (
     <>
-    <nav>
-    Tell Us What You Think
-    </nav>
-      <form className="feedback-form">
-        <h2>We'd Love to Hear From You!</h2>
-        <p>Please share your feedback with us.</p>
+        <nav>
+        Tell Us What You Think
+        </nav>
+        <form className="feedback-form">
+            <h2>We'd Love to Hear From You!</h2>
+            <p>Please share your feedback with us.</p>
 
-        <input
-            type = "text"
-            name = "name"
-            placeholder = "Your Name"    
-        />
+            <input
+                type = "text"
+                name = "name"
+                placeholder = "Your Name"    
+            />
 
-        <input
-            type = 'email'
-            name = 'email'
-            placeholder = 'Your Email'
-        />
+            <input
+                type = 'email'
+                name = 'email'
+                placeholder = 'Your Email'
+            />
 
-        <textarea 
-            name = 'feedback'
-            placeholder = 'Your Feedback'
-        ></textarea>
+            <textarea 
+                name = 'feedback'
+                placeholder = 'Your Feedback'
+            ></textarea>
 
-        <button type='submit'>Submit Feedback</button>
-        
-      </form>
+            <button type='submit'>Submit Feedback</button>
+            
+        </form>
     </>
   );
 };
